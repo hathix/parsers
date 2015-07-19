2ed34cf3843197b6b395a15e7f9b141bb70ab68d
Neel Mehta <neelmehta@college.harvard.edu>
2015-07-18 20:27:58 -0700

    Added parsing using parse table

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var Immutable = require('immutable');
+var Baobab = require('baobab');
 
 /* LL(1) parser, from https://en.wikipedia.org/wiki/LL_parser
  */
@@ -34,7 +36,7 @@ _.mixin({
  * cellFn(rowValue, columnValue).
  */
 function generateTable2d(rowValues, rowLabelFn, columnValues,
-        columnLabelFn, cellFn) {
+    columnLabelFn, cellFn) {
     var table = {};
     _.each(rowValues, function(rowValue) {
         var currentRow = table[rowLabelFn(rowValue)] = {};
@@ -211,13 +213,147 @@ var parseTable = generateTable2d(nonterminalData, function(nonterminal) {
     var validRules = _.filter(nonterminalRules, function(rule) {
         return _.includes(rule.first, terminal) ||
             (_.includes(rule.first, EMPTY) &&
-            _.includes(nonterminal.follow, terminal));
+                _.includes(nonterminal.follow, terminal));
     });
     if (_.isEmpty(validRules)) {
+        // there is no defined behavior for [A, a]
         return null;
     } else {
+        // validRules should only have 1 element, so grab it; this element
+        // is a production rule A => w
+        // TODO: throw error if validRules has >1 elements
         return _.head(validRules);
     }
 });
 
-console.log(JSON.stringify(parseTable));
+// console.log(JSON.stringify(parseTable));
+
+/**
+ * Utility constructor for creating tree nodes with specified values and
+ * (by default) an empty list of children.
+ */
+function TreeNode(value) {
+    this.value = value;
+    this.children = [];
+}
+
+// Utility Baobab functions
+/**
+ * For TreeNodes in a Baobab tree.
+ * Returns the node immediately to the right of the given node. But if the node
+ * is already the rightmost child of its parent, returns the sibling of its
+ * parent.
+ */
+function siblingNode(node) {
+    if (node.isRoot()) {
+        // this node is the root of the tree; just return itself
+        // TODO: throw error?
+        return node;
+    } else if (node === node.rightmost()) {
+        // this node is already the rightmost; try going right on its parent
+        // this requires two "up"'s -- one to get from the node to the list of
+        // children of its parent, and another to get to the parent itself
+        return siblingNode(node.up().up());
+    } else {
+        // standard case -- return the node to the right
+        return node.right();
+    }
+}
+
+/**
+ * For TreeNodes in a Baobab tree.
+ * Maps the given list of values to a list of new TreeNodes, attaches those
+ * as new children of the given node, and returns the node. This mutates
+ * the given node.
+ */
+function addChildren(node, valueList) {
+    var children = _.map(valueList, function(value) {
+        return new TreeNode(value);
+    });
+    node.select("children").push(children);
+    return node;
+}
+
+/**
+ * Given a list of terminal symbols, parses it into the abstract syntax tree
+ * that generated this list. The list should have the first symbol to be
+ * parsed at the front.
+ */
+function parse(rawInputList) {
+    // we have a list of input symbols (must be terminated with the end
+    // character for our parsing to work) and a stack representing the current
+    // state of the AST, which naturally starts out with the start symbol
+    // (which represents the full sentence and the head of the AST)
+    var baseInput = Immutable.List(rawInputList).push(END);
+    var baseStack = Immutable.Stack.of(START);
+    // our output will be an abstract syntax tree, the root node of which is
+    // the start symbol
+    var baseTree = new Baobab(new TreeNode(START), {
+        // Baobab options
+        asynchronous: false,
+        // immutable: true
+    });
+
+    /**
+     * Parses the given input (Immutable.List) using the given stack
+     * (Immutable.Stack) and uses this to build up the given abstract syntax
+     * tree (Baobab).
+     * The given tree should be a Baobab cursor; if you have a Baobab tree
+     * just access its "root" field.
+     * The root of the given tree should have the same value
+     * as the element at the top of the stack (i.e. the given tree gets built
+     * recursively.
+     */
+    var parseHelper = function(input, stack, tree) {
+        // console.log(tree.get());
+        if (stack.isEmpty()) {
+            // done parsing
+            // if the original input was valid, input should be no more than
+            // the end symbol
+            // since we're finished, return the finished tree
+            return tree;
+        } else {
+            var topStackSymbol = stack.first();
+            var topInputSymbol = input.first();
+
+            if (topStackSymbol === topInputSymbol) {
+                // if the stack and input symbols match, they can be removed;
+                // the current node in the tree is a leaf, so move on to its
+                // sibling
+                return parseHelper(
+                    input.shift(),
+                    stack.pop(),
+                    siblingNode(tree));
+            } else if (topStackSymbol === EMPTY) {
+                // an empty symbol can simply be removed from the stack
+                // because it is, well, empty
+                // the empty symbol is a leaf, so move on to its sibling
+                // -- which will be the sibling of its parent because the
+                // empty sibling is always the only child of its parent and,
+                // therefore, the rightmost sibling thereof
+                return parseHelper(
+                    input,
+                    stack.pop(),
+                    siblingNode(tree));
+            } else {
+                // look up the matching rule in the parse table and place the
+                // right side thereof on the stack
+                // also add all the symbols on the right side to the tree
+                // and move into the leftmost new child (which happens by
+                // default when you drill down into a tree)
+                var rule = parseTable[topStackSymbol][topInputSymbol];
+                return parseHelper(
+                    input,
+                    stack.pop().pushAll(rule.right),
+                    addChildren(tree, rule.right).select('children').down().leftmost());
+            }
+        }
+    };
+
+    return parseHelper(baseInput, baseStack, baseTree.root);
+
+    // TODO: check that the input list contains only valid symbols
+}
+
+var ast = parse("aacbbcb".split(""));
+console.log(JSON.stringify(ast));
